2e97f31eb54252c6576ac182bf2f874c04a0f4e7
Shrikant Pardhi <shripardhi92@gmail.com>
2025-12-11 12:37:59 +0530

    Refactor MaskingMessageConverter to SensitiveDataMessageConverter

--- src/main/java/com/utility/masking/converter/MaskingMessageConverter.java
+++ src/main/java/com/utility/masking/converter/MaskingMessageConverter.java
@@ -1,30 +1,76 @@
-package com.utility.masking.converter;
+package com.utility.masking.slf4j;
 
+import ch.qos.logback.classic.pattern.MessageConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.utility.masking.annotation.Sensitive;
+import com.utility.masking.service.EnvironmentChecker;
 import com.utility.masking.service.MaskingService;
-import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
 
 /**
- * Message converter that automatically masks sensitive data when objects are logged
+ * Custom SLF4J message converter that automatically masks sensitive data
  * 
- * This integrates with SLF4J's toString() mechanism to ensure that whenever
- * an object with @Sensitive fields is logged, those fields are automatically masked.
+ * Configuration in logback.xml:
+ * <pre>
+ * &lt;conversionRule conversionWord="maskedMsg" 
+ *                 converterClass="com.utility.masking.slf4j.SensitiveDataMessageConverter" /&gt;
+ * 
+ * &lt;pattern&gt;%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %maskedMsg%n&lt;/pattern&gt;
+ * </pre>
  */
-@Component
-public class MaskingMessageConverter {
+public class SensitiveDataMessageConverter extends MessageConverter {
 
-    private final MaskingService maskingService;
+    private static MaskingService maskingService;
+    private static EnvironmentChecker environmentChecker;
 
-    public MaskingMessageConverter(MaskingService maskingService) {
-        this.maskingService = maskingService;
+    public static void setMaskingService(MaskingService service) {
+        maskingService = service;
     }
 
-    /**
-     * Converts an object to a masked string representation
-     * 
-     * @param obj the object to convert
-     * @return masked string representation
-     */
-    public String convert(Object obj) {
-        return maskingService.maskData(obj);
+    public static void setEnvironmentChecker(EnvironmentChecker checker) {
+        environmentChecker = checker;
+    }
+
+    @Override
+    public String convert(ILoggingEvent event) {
+        // If masking is disabled, use default behavior
+        if (maskingService == null || environmentChecker == null || !environmentChecker.shouldMask()) {
+            return super.convert(event);
+        }
+
+        // Get the formatted message
+        String formattedMessage = event.getFormattedMessage();
+        Object[] argumentArray = event.getArgumentArray();
+
+        if (argumentArray == null || argumentArray.length == 0) {
+            return formattedMessage;
+        }
+
+        // Replace sensitive arguments with masked versions
+        String result = formattedMessage;
+        for (Object arg : argumentArray) {
+            if (arg != null && hasSensitiveFields(arg.getClass())) {
+                String original = arg.toString();
+                String masked = maskingService.maskData(arg);
+                result = result.replace(original, masked);
+            }
+        }
+
+        return result;
+    }
+
+    private boolean hasSensitiveFields(Class<?> clazz) {
+        if (clazz == null || clazz == Object.class) {
+            return false;
+        }
+
+        for (Field field : clazz.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Sensitive.class)) {
+                return true;
+            }
+        }
+
+        return hasSensitiveFields(clazz.getSuperclass());
     }
 }
